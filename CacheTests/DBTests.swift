d53bb7e1db49b5a02b7cfd761650d08919220fcd
丁帅 <ding3725371@hotmail.com>
2017-05-16 17:40:11 +0800

    update DBTest

--- CacheTests/DBTests.swift
+++ CacheTests/DBTests.swift
@@ -13,15 +13,32 @@ import CoreData
 class DBTests: XCTestCase {
     
     func testSave() {
-        
-        let cache = CacheObject.insert()
-        cache.key = "foo"
-        cache.size = 123456
-        cache.path = "/foo"
-        CacheObject.save()
+        let cache: CacheObject
+        if let _cache = CacheObject.get(with: "foo") {
+            cache = _cache
+            print("get:  ", cache.last_update_time as Any)
+            cache.last_update_time = Date() as NSDate
+            CacheObject.save()
+        } else {
+            cache = CacheObject.insert()
+            cache.key = "foo"
+            cache.size = 123456
+            cache.path = "/foo"
+            cache.last_update_time = Date() as NSDate
+            print("insert:  ", cache.last_update_time!)
+            CacheObject.save()
+        }
         
         let result = CacheObject.get(with: "foo")
         XCTAssertNotNil(result)
         XCTAssertEqual(result?.path, cache.path)
+        
+        let delete = CacheObject.remove(with: "foo")
+        CacheObject.save()
+        XCTAssertNotNil(delete)
+        XCTAssertEqual(delete?.path, result?.path)
+        
+        let beNil = CacheObject.get(with: "foo")
+        XCTAssertNil(beNil)
     }
 }
