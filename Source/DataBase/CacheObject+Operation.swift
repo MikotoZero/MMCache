afadaa3f10d23500523be0ce62b605d5e436479b
丁帅 <ding3725371@hotmail.com>
2017-06-02 16:17:45 +0800

    fix bundle cannot loaded with bundle identifier when use it with Cocoapods

--- Source/DataBase/CacheObject+Operation.swift
+++ Source/DataBase/CacheObject+Operation.swift
@@ -21,8 +21,7 @@ private class CacheDBContext {
     
     static var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         guard
-            let bundle = Bundle(identifier: "com.MikotoZero.MMCache"),
-            let modelURL = bundle.url(forResource: "MMCache", withExtension: "momd"),
+            let modelURL = Bundle(for: MMCache.CacheObject).url(forResource: "MMCache", withExtension: "momd"),
             let model = NSManagedObjectModel(contentsOf: modelURL)
             else {
                 fatalError("MMCache.DiskCache.CoreData Error: Load DB cache file fail.")
